6cf5631545c43fb193362b7ea979d11675a6355b
Haluk Akbay <64612753+HAkbay@users.noreply.github.com>
2023-07-27 19:05:44 +0300

    Clarification about the question

--- Q1768-Solution.cs
+++ Q1768-Solution.cs
@@ -40,3 +40,13 @@ namespace LeetCode_Solutions
         }
     }
 }
+
+/*
+Question is as follows:
+
+You are given two strings word1 and word2.
+Merge the strings by adding letters in alternating order, starting with word1.
+If a string is longer than the other, append the additional letters onto the end of the merged string.
+
+Return the merged string.
+*/
